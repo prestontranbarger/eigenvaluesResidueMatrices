6ebe64935a1b461c2140b67f4110db7aa3d64460
Preston Tranbarger <preston.tranbarger@gmail.com>
2021-11-01 18:44:14 -0500

    Update driverCubicBFM.py

--- driverCubicBFM.py
+++ driverCubicBFM.py
@@ -2,17 +2,15 @@ from baseCubic import *
 from readWriteBFM import *
 
 bfmPath = "C:\\Users\\Preston\\Documents\\TAMU\\Courses\\F21\\MATH 491.201\\output\\3\\square\\BFCM9000.txt"
-RWBFM = rwBFM()
-RWBFM.setReadPath(bfmPath)
-RWBFM.setWritePath(bfmPath)
+RWBFM = rwBFM(bfmPath)
 
-maxNorm = 1000
+maxNorm = 5000
 vE = computeViableElementsCubic(maxNorm)
 vEGuide = [toStringEisenstein(alpha) for alpha in vE]
 
 m = []
 i = 0
-for a in vE:
+for a in tqdm(vE):
     i += 1
     m.append([toStringEisenstein(extendedCubicResidueSymbol(a, b)[0]) for b in vE])
     print(str(i) + "/" + str(len(vE)))
