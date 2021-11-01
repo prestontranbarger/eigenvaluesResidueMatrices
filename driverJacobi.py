5f7248a4d127f2d8d9df9552c76afd78e5f6e437
Preston Tranbarger <preston.tranbarger@gmail.com>
2021-11-01 13:41:02 -0500

    Update driverJacobi.py

--- driverJacobi.py
+++ driverJacobi.py
@@ -2,16 +2,18 @@ from baseJacobi import *
 from readWriteEvs import *
 
 evsPath = "C:\\Users\\Preston\\Documents\\TAMU\\Courses\\F21\\MATH 491.201\\output\\2\\square\\evs.txt"
-normEvsPath = "C:\\Users\\Preston\\Documents\\TAMU\\Courses\\F21\\MATH 491.201\\output\\2\\square\\normEvs.txt"
+normEvsPath = "C:\\Users\\Preston\\Documents\\TAMU\\Courses\\F21\\MATH 491.201\\output\\2\\square\\nevs.txt"
 imgPath = "C:\\Users\\Preston\\Documents\\TAMU\\Courses\\F21\\MATH 491.201\\output\\2\\square\\image\\"
 
 RWEvs = rwEvs()
 
-maxNorms = list(range(3001, 4001, 50))
+maxNorms = list(range(10001, 25001, 100))
 evNums = set()
 evsArray = []
 normEvsArray = []
+bT = time.time()
 for maxNorm in maxNorms:
+    print("Compute Elements: ", end = "")
     vE = computeViableElementsJacobi(maxNorm)
     if(len(vE) not in evNums):
         print(maxNorm)
@@ -19,6 +21,7 @@ for maxNorm in maxNorms:
         RWEvs.setReadPath(evsPath)
         rEvs = RWEvs.readEvs(len(vE))
         if(rEvs == -1):
+            print("Construct Matrix: ", end = "")
             m = constructMatrixJacobi(vE)
             svdDecomp = svd(m)
             evs = extractEigenvals(svdDecomp, len(vE))
@@ -29,7 +32,9 @@ for maxNorm in maxNorms:
             RWEvs.setWritePath(normEvsPath)
             RWEvs.writeEvs(evs[1][:])
         else:
+            print("Eigenvalues Found")
             evsArray.append(rEvs)
             RWEvs.setReadPath(normEvsPath)
             normEvsArray.append(RWEvs.readEvs(len(vE)))
-createPlot(evsArray, normEvsArray, True, 5, imgPath)
+print(time.time() - bT)
+createPlot(evsArray, normEvsArray, False, 5, imgPath)
