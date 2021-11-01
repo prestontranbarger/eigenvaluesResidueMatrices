7cd6e21181b38556c3b4a761378d9ea5215c3e70
Preston Tranbarger <preston.tranbarger@gmail.com>
2021-11-01 18:45:22 -0500

    Update readWriteBFM.py

--- readWriteBFM.py
+++ readWriteBFM.py
@@ -1,3 +1,5 @@
+from tqdm import tqdm
+
 class rwBFM:
     def __init__(self, readPath = None ,writePath = None):
         self.readPath = readPath
@@ -20,23 +22,43 @@ class rwBFM:
         self.writePath = writePath
         return 0
 
-    def readBFM(self, rows, cols):
+    def readBFMGuide(self):
+        #not optimal for large files
         file = open(self.readPath, 'r')
         lines = file.readlines()
         file.close()
 
-        m = []
-        for row in rows[:-2]:
-            rowEntries = lines[row][1:-2].replace("\'","").split(", ")
-            rowAppend = [rowEntries[col] for col in cols]
-            m.append(rowAppend)
         rowGuide = lines[-2][1:-2].replace("\'","").split(", ")
         colGuide = lines[-1][1:-2].replace("\'","").split(", ")
-        return m, rowGuide, colGuide
+
+        return rowGuide, colGuide
+
+    def guideToDict(self, guide):
+        guideDict = {"": -1}
+        for i in range(len(guide)):
+            guideDict[guide[i]] = i
+        return guideDict
+
+    def readBFM(self, getGuide = False, rows = None, cols = None):
+        m = []
+        rowGuide = []
+        colGuide = []
+        with open(self.readPath) as file:
+            for line in tqdm(file):
+                if not getGuide:
+                    rowEntries = line[1:-2].replace("\'", "").split(", ")
+                    rowAppend = [rowEntries[col] for col in range(len(rowEntries))]
+                    m.append(rowAppend)
+                rowGuide = colGuide
+                colGuide = line[1:-2].replace("\'", "").split(", ")
+        if getGuide:
+            return rowGuide, colGuide
+        else:
+            return m[:-2], rowGuide, colGuide
 
     def writeBFM(self, matrix, rowGuide, colGuide):
         file = open(self.writePath, 'a')
-        file.writelines([str(row)+"\n" for row in matrix])
+        file.writelines([str(row)+"\n" for row in tqdm(matrix)])
         file.writelines(str(rowGuide)+"\n")
         file.writelines(str(colGuide)+"\n")
         file.close()
