4a783df646282f63315f6c81967830ed73169604
Preston Tranbarger <preston.tranbarger@gmail.com>
2021-11-01 18:45:03 -0500

    Update driverJacobiBFM.py

--- driverJacobiBFM.py
+++ driverJacobiBFM.py
@@ -2,16 +2,14 @@ from baseJacobi import *
 from readWriteBFM import *
 
 bfmPath = "C:\\Users\\Preston\\Documents\\TAMU\\Courses\\F21\\MATH 491.201\\output\\2\\square\\BFQM9000.txt"
-RWBFM = rwBFM()
-RWBFM.setReadPath(bfmPath)
-RWBFM.setWritePath(bfmPath)
+RWBFM = rwBFM(bfmPath)
 
 maxNorm = 100000
 vE = computeViableElementsJacobi(maxNorm)
 
 m = []
 counter = 0
-for a in vE:
+for a in tqdm(vE):
     counter += 1
     m.append([jacobiSymbol(a, b)[0] for b in vE])
     print(str(counter) + "/" + str(len(vE)))
